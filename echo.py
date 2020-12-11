c873264cc67ccc5c5133edeb6214a1f4645ac01e
Kamela Williamson <kami63116@yahoo.com>
2020-12-11 15:28:31 -0700

    added author and resources

--- echo.py
+++ echo.py
@@ -3,7 +3,14 @@
 """An enhanced version of the 'echo' cmd line utility."""
 
 __author__ = "kamela williamson"
-# resources
+# Daniel's demos for tdd and argparse
+# my assessments and tests
+# https://www.freecodecamp.org/news/learning-to-test-with-python-997ace2d8abe/
+# https://realpython.com/python-testing/
+# https://www.youtube.com/watch?v=6tNS--WetLI&feature=emb_title
+# https://docs.python.org/3/library/unittest.html#module-unittest
+# https://docs.python.org/3/library/unittest.html#basic-example
+# https://docs.python.org/3/library/unittest.html#assert-methods
 
 import sys
 
